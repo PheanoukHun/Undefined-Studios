2b29a2959f08f5f5c9d4cb125b6d721dd16c7ab9
PheanoukHun <pheanouk.hun@student.nisdtx.org>
2024-04-22 20:12:54 -0500

    Changes

--- Classes/character.py
+++ Classes/character.py
@@ -1,68 +1,83 @@
 import pygame
+import os
+import json
 from sprite import AnimatedSprites
 
 pygame.init()
 
 class Player():
-    def __init__(self, player_type, level, window):
-        self.sprites = {}
-        self.sprite = None
+    def __init__(self, player_type, window, screen_width, screen_height, x, y):
+        with open(f"CharacterAssets\{player_type}\CharacterInfo.json") as json_file:
+            self.data = json.load(json_file)
+        
+        self.x = x
+        self.y = y
+        
         self.health = 0
         self.attack_damage = 0
         self.state = "Idle"
         self.direction = "East"
         self.attack_key_pressed = False
-        self.VEL = 0
+        self.VEL = self.data[self.state]["VEL"]
         self.window = window
+        self.window_width = screen_width
+        self.window_height = screen_height
+        
+        self.sprite = AnimatedSprites(self.x, self.y, self.data[self.state]["Height"], self.data[self.state]["Width"], self.data[self.state][self.direction][0])
+        self.update_sprite_actions()
     
-    def create_sprites(self):
-        pass
+    def update_sprite_actions(self):
+        self.sprite = AnimatedSprites(self.x, self.y, self.data[self.state]["Height"], self.data[self.state]["Width"], self.data[self.state][self.direction][0])
+        self.sprite.sprite_image
 
-    def handle_character_states(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                self.state = "Walking"
-                self.direction = "North"
-                self.sprite.y_speed = -self.VEL
-            if event.key == pygame.K_s:
-                self.state = "Walking"
-                self.direction = "South"
-                self.sprite.y_speed = self.VEL
-            if event.key == pygame.K_a:
-                self.state = "Walking"
-                self.direction = "West"
-                self.sprite.x_speed = -self.VEL
-            if event.key == pygame.K_d:
-                self.state = "Walking"
-                self.direction = "East"
-                self.sprite.x_speed = self.VEL
+    def update_info(self):
+        self.VEL = self.data[self.state]["VEL"]
+        self.sprite.image = self.sprites[self.state][self.direction]
+        self.sprite.animation_speed = self.data[self.state]["AnimationSpeed"]
 
-            if event.key == pygame.K_UP:
-                self.state = "Slashing"
-                self.direction = "North"
-                self.sprite.x_speed = 0
-                self.sprite.y_speed = 0
-            if event.key == pygame.K_DOWN:
-                self.state = "Slashing"
-                self.direction = "South"
-                self.sprite.x_speed = 0
-                self.sprite.y_speed = 0
-            if event.key == pygame.K_RIGHT:
-                self.state = "Slashing"
-                self.direction = "East"
-                self.sprite.x_speed = 0
-                self.sprite.y_speed = 0
-            if event.key == pygame.K_RIGHT:
-                self.state = "Slashing"
-                self.direction = "WEST"
-                self.sprite.x_speed = 0
-                self.sprite.y_speed = 0
+    def handle_character_states(self, keys_pressed):
+        if keys_pressed[pygame.K_w]:
+            self.state = "Walking"
+            self.direction = "North"
+            self.update_info()
+            self.sprite.y_speed = self.VEL
+        if keys_pressed[pygame.K_s]:
+            self.state = "Walking"
+            self.direction = "South"
+            self.update_info()
+            self.sprite.y_speed = -self.VEL
+        if keys_pressed[pygame.K_a]:
+            self.state = "Walking"
+            self.direction = "West"
+            self.update_info()
+            self.sprite.x_speed = -self.VEL
+        if keys_pressed[pygame.K_d]:
+            self.state = "Walking"
+            self.direction = "East"
+            self.update_info()
+            self.sprite.x_speed = self.VEL
+
+        if keys_pressed[pygame.K_UP]:
+            self.state = "Slashing"
+            self.direction = "North"
+            self.sprite.x_speed = 0
+            self.sprite.y_speed = 0
+        if keys_pressed[pygame.K_DOWN]:
+            self.state = "Slashing"
+            self.direction = "South"
+            self.sprite.x_speed = 0
+            self.sprite.y_speed = 0
+        if keys_pressed[pygame.K_LEFT]:
+            self.state = "Slashing"
+            self.direction = "East"
+            self.sprite.x_speed = 0
+            self.sprite.y_speed = 0
+        if keys_pressed[pygame.K_RIGHT]:
+            self.state = "Slashing"
+            self.direction = "WEST"
+            self.sprite.x_speed = 0
+            self.sprite.y_speed = 0
     
-
-
     def draw(self, keys_pressed):
-        if self.state == "Idle":
-            self.sprite.animation_speed = 999999999999999999999
-
-        self.handle_character_states
+        self.handle_character_states(keys_pressed)
         self.window.blit(self.sprite.sprite_image, (self.sprite.sprite_rect.x, self.sprite.sprite_rect.y))
