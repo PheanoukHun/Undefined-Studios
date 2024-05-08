a3cf2f8ddaa2f969d3f8a39a6aa1270d29264b68
PheanoukHun <pheanouk.hun@student.nisdtx.org>
2024-05-08 14:24:15 -0500

    Changes

--- Main/level.py
+++ Main/level.py
@@ -94,7 +94,6 @@ class Level:
         self.visible_sprites.enemy_update(self.player, [self.enemy_attack_sprites])
         self.player_attack_logic()
         self.mob_attack_logic()
-        self.player.update()
         self.ui.display(self.player)
 
 class YSortCameraGroup(pygame.sprite.Group):
